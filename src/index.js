b16f8956ec1ac43db7d85bd5e59b3c34f30b321d
Roman Kurashevich <rkurashevich@gmail.com>
2021-02-22 00:46:08 +0700

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    // let str = String(n);
+    // let arr = str.split("");
+    // let revarr = arr.reverse();
+    // let newstr = revarr.join("");
+    // let result = Number(newstr)
+    // return result
+    return Number(String(n).split("").reverse().filter(item => item !='-').join(""))
 }
